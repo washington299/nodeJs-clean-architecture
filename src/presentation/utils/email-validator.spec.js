457126aa594ee7cbb0d83dffd2b2c4d1eb75b5b7
Washington Campos <washingtoncampos9@gmail.com>
2020-01-30 01:00:48 -0300

    move EmailValidator sut to a factory method

--- src/presentation/utils/email-validator.spec.js
+++ src/presentation/utils/email-validator.spec.js
@@ -6,9 +6,13 @@ class EmailValidator {
   }
 }
 
+const makeSut = () => {
+  return new EmailValidator();
+};
+
 describe("Email validator", () => {
   test("Should return true if validator returns true", () => {
-    const sut = new EmailValidator();
+    const sut = makeSut();
     const isEmailValid = sut.isValid("random@mail.com");
 
     expect(isEmailValid).toBe(true);
@@ -16,7 +20,7 @@ describe("Email validator", () => {
 
   test("Should return false if validator returns false", () => {
     validator.isEmailValid = false;
-    const sut = new EmailValidator();
+    const sut = makeSut();
     const isEmailValid = sut.isValid("invalid_random@mail.com");
 
     expect(isEmailValid).toBe(false);
